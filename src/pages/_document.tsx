6f09bbca2c776bdc79afbeae05d0c977a05023ef
antoinechalifour <antoine.chalifour@gmail.com>
2020-06-11 19:13:34 +0200

    feat: add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -34,6 +34,24 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
+          {process.env.GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.GA_TRACKING_ID}');
+                    `,
+                }}
+              />
+            </>
+          )}
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
